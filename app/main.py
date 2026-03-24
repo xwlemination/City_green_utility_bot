487b763fb1a514b82802bf3d25e41a31d95ebe07
xwlemination <eina.johnson@cloudempower.io>
2026-03-24 16:01:53 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -45,5 +45,7 @@ async def handle_lex(request: Request):
                 "error_flag": "true"
             }
         }
+
+if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
